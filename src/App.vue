28623155c42a36736746f4f2615271aaae39c2ed
Muhammad Raza. Bangi <raza.bangi@callcenter.com>
2023-10-09 14:27:16 -0700

    todo app created

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>Todo Count: {{ todoCount }}</h2>
+  <ul>
+    <li v-for="{ todo } in todos" :key="todo">{{ todo }} <button @click="removeTodo(todo)">X</button></li>
+  </ul>
+  <input type="text" v-model.trim="newTodo">
+  <button @click="addTodo">Add Todo</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTodo: "Forth Task",
+      todos: [
+        { todo: "First task" },
+        { todo: "Second task" },
+        { todo: "Third task" },
+      ],
+    };
+  },
+  methods: {
+    addTodo () {
+      if (this.newTodo !== '') {
+        this.todos.push({ todo: this.newTodo });
+        this.newTodo = ''
+      }
+    },
+    removeTodo (todo) {
+      this.todos = this.todos.filter(e => e.todo != todo);
+    }
+  },
+  computed: {
+    todoCount() {
+      return this.todos.length;
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
